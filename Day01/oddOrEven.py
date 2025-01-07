62071e5b44e9d5d89567c788bf4c31c6486648ab
shiva <shivananda44@gmail.com>
2025-01-07 23:28:00 +0530

    oddoreven prg solved

---
+++ Day01/oddOrEven.py
@@ -0,0 +1,13 @@
+# 1. Python program to check whether the given number is even or not.
+
+
+def oddoreven():
+    givennumber = int(input("Enter your number \n"))
+    if givennumber % 2 == 0 :
+        print("Given number is EVEN")
+    else:
+        print("Given number is ODD")
+
+oddoreven()
+
+
