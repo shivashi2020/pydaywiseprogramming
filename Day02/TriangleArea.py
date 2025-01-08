25ce52d2ee1a208f64701e7b399c699d1b65c3a3
shiva <shivananda44@gmail.com>
2025-01-08 23:15:03 +0530

    temp in deggree to Fahrenheit added

--- Day02/TriangleArea.py
+++ Day02/TriangleArea.py
@@ -2,11 +2,14 @@
 
 #To find the area of a triangle when only the lengths of its three sides are known, you can use Heron's formula:.
 
-
 import math
-a = float(input("Enter the length of side a: "))
-b = float(input("Enter the length of side b: "))
-c = float(input("Enter the length of side c: "))
-s = (a+b+c)/2
-area = math.sqrt(s*(s-a)*(s-b)*(s-c))
-print("Area of the triangle is: ", area)
+
+try:
+    a = float(input("Enter the length of side a: "))
+    b = float(input("Enter the length of side b: "))
+    c = float(input("Enter the length of side c: "))
+    s = (a+b+c)/2
+    area = math.sqrt(s*(s-a)*(s-b)*(s-c))
+    print("Area of the triangle is: ", area)
+except:
+    print("Please enter a valid numerical value.")
