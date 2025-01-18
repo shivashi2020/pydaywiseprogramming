e69ef6557408fbdb7face8cfe850be0a2e50c030
shivashi2020 <73793621+shivashi2020@users.noreply.github.com>
2025-01-18 23:28:25 +0530

    Update swapno.py
    update added

--- Day05/swapno.py
+++ Day05/swapno.py
@@ -4,8 +4,8 @@ x = 5
 y = 10
 
 # To take inputs from the user
-#x = input('Enter value of x: ')
-#y = input('Enter value of y: ')
+x = input('Enter value of x: ')
+y = input('Enter value of y: ')
 
 # create a temporary variable and swap the values
 temp = x
